130d18b7082fd3e6b32c7e39f93b283036b635ca
Ivgesha <evgenise93@gmail.com>
2021-05-08 16:58:22 +0300

    Add project files.

---
+++ mySecondProgram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace mySecondProgram
+{
+    class Program
+    {
+        const int port = 8080;
+        static void Main(string[] args) {
+
+            try
+            {
+                if (!HttpListener.IsSupported)
+                {
+                    Console.WriteLine("Wow dude chill");
+                    return;
+                }
+                // create listener
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:8080/");
+                listener.Start();
+                Console.WriteLine("Listening...");
+
+                HttpListenerContext context = listener.GetContext();        // Blocking line
+                HttpListenerRequest request = context.Request;
+
+
+                Console.WriteLine("passed blocking line ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        
+            Console.ReadLine();
+        
+        }
+
+
+    }
+    
+}
