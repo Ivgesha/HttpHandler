cd47c74c402a9bcb22f4fb52d42d211ae0a7656d
Ivgesha <evgenise93@gmail.com>
2021-05-08 17:32:18 +0300

    Added http requests + response + whilte true loop.

--- mySecondProgram/Program.cs
+++ mySecondProgram/Program.cs
@@ -7,50 +7,54 @@ namespace mySecondProgram
 {
     class Program
     {
-        const int port = 8080;
-        static void Main(string[] args) {
+        static string url;
+        static void Main(string[] args)
+        {
             string responseString;
-            try
+            url = "http://localhost:8080/";
+            
+
+            while (true)
             {
-                if (!HttpListener.IsSupported)
+                try
                 {
-                    Console.WriteLine("Your machine dosent support HttpLIstener class.");
-                    return;
+                    if (!HttpListener.IsSupported)
+                    {
+                        Console.WriteLine("Your machine dosent support HttpLIstener class.");
+                        return;     // exiting the loop. 
+                    }
+                    // create listener.
+                    HttpListener listener = new HttpListener();
+                    // the url and port we are listening to.
+                    listener.Prefixes.Add(url);
+                    listener.Start();
+                    Console.WriteLine("Listening...");
+
+                    // get context - a blocking line that waiting for http request to arrive.
+                    HttpListenerContext context = listener.GetContext();        // Blocking 
+                    HttpListenerRequest request = context.Request;
+                    Console.WriteLine("Got requests, preparing response.");
+                    // prepare response object
+                    HttpListenerResponse response = context.Response;
+                    // prepare response
+                    responseString = "<HTML><BODY> Hello From C#!</BODY></HTML>";
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                    // get the output respinse stream and write the response.
+                    response.ContentLength64 = buffer.Length;
+                    response.StatusCode = 200;
+                    System.IO.Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+
+                    output.Close();     // close output stream
+                    listener.Stop();    // stip listening to http requests
                 }
-                // create listener.
-                HttpListener listener = new HttpListener();
-                // the url and port we are listening to.
-                listener.Prefixes.Add("http://localhost:8080/");
-                listener.Start();
-                Console.WriteLine("Listening...");
+                catch (Exception e)
+                {
+                    Console.WriteLine("Fail: " + e.Message);
 
-                // get context - a blocking line that waiting for http request to arrive.
-                HttpListenerContext context = listener.GetContext();        // Blocking 
-                HttpListenerRequest request = context.Request;
-                // prepare response object
-                HttpListenerResponse response = context.Response;
-                // prepare response
-                responseString = "<HTML><BODY> Hello From C#!</BODY></HTML>";
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-
-                // get the output respinse stream and write the response.
-                response.ContentLength64 = buffer.Length;
-                System.IO.Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-
-                output.Close();     // close output stream
-                listener.Stop();    // stip listening to http requests
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: " + e.Message);
-            }
-        
-            Console.ReadLine();
-        
         }
-
-
     }
-    
 }
