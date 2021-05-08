6ad4b5b7a597c1916196b26ff5ff04c801965f9d
Ivgesha <evgenise93@gmail.com>
2021-05-08 17:11:44 +0300

    Added respose to incoming http requests.

--- mySecondProgram/Program.cs
+++ mySecondProgram/Program.cs
@@ -14,23 +14,33 @@ namespace mySecondProgram
             {
                 if (!HttpListener.IsSupported)
                 {
-                    Console.WriteLine("Wow dude chill");
+                    Console.WriteLine("Your machine dosent support HttpLIstener class.");
                     return;
                 }
-                // create listener
+                // create listener.
                 HttpListener listener = new HttpListener();
+                // the url and port we are listening to.
                 listener.Prefixes.Add("http://localhost:8080/");
                 listener.Start();
                 Console.WriteLine("Listening...");
 
-                HttpListenerContext context = listener.GetContext();        // Blocking line
+                // get context - a blocking line that waiting for http request to arrive.
+                HttpListenerContext context = listener.GetContext();        // Blocking 
                 HttpListenerRequest request = context.Request;
-
+                // prepare response object
+                HttpListenerResponse response = context.Response;
+                // prepare response
                 responseString = "<HTML><BODY> Hello From C#!</BODY></HTML>";
-                Console.WriteLine("passed blocking line ");
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
 
+                // get the output respinse stream and write the response.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
 
-             }
+                output.Close();     // close output stream
+                listener.Stop();    // stip listening to http requests
+            }
             catch (Exception e)
             {
                 Console.WriteLine("Error: " + e.Message);
