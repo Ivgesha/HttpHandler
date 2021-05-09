fe07c77ce5ded05a2814b72a22e24c0f0c96d952
Ivgesha <evgenise93@gmail.com>
2021-05-09 22:42:12 +0300

    db insert and conenction

--- mySecondProgram/DBConnection.cs
+++ mySecondProgram/DBConnection.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;   // sql data base 
-
+ 
 namespace mySecondProgram
 {
 
@@ -26,9 +26,10 @@ namespace mySecondProgram
         private string uid;
         private string password;
 
-
+        // todo: check me out 
+        // https://www.codeproject.com/Articles/43438/Connect-C-to-MySQL
         private void initialize() {
-            server = "localhost";
+        server = "localhost";
             //database = "connect csharp to mysql";
             database = "Local instance MySQL80";
             //uid = "username";
@@ -41,9 +42,100 @@ namespace mySecondProgram
             connection = new MySqlConnection(connectionString);
 
         }
+        //open connection to database
 
+        private bool openConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
 
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
 
+        //Close connection
+        private bool closeConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (Exception e) {
+                Console.WriteLine("Cannot close db connection: " + e.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert()
+        {
+            string str = "Insert into http_requests_history (end_point,method,header,body)values(@endPoint,@method,@header,@body)";
+            if (this.openConnection() == true) {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = str;
+                cmd.Parameters.AddWithValue("@endPoint", "/EndPoint");
+                cmd.Parameters.AddWithValue("@method", "Get");
+                cmd.Parameters.AddWithValue("@header", "Headers.");
+                cmd.Parameters.AddWithValue("@body", "body");
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+
+    
+            }
+
+        }
+        /*
+        //Update statement
+        public void Update()
+        {
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+        }
+
+        //Select statement
+        public List<string>[] Select()
+        {
+        }
+
+        //Count statement
+        public int Count()
+        {
+        }
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
+
+    */
 
     }
 }
